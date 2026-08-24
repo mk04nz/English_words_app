15851b2d34fa82003e06ea5aeb24e5b3eb7dc617
mk04nz <p.mkmk12@gmail.com>
2026-08-24 20:01:34 +1200

    Initial Commit

---
+++ EnglishWordsApp/EnglishWordsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishWordsAppApp.swift
+//  EnglishWordsApp
+//
+//  Created by mami kojima on 2026/08/24.
+//
+
+import SwiftUI
+
+@main
+struct EnglishWordsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
